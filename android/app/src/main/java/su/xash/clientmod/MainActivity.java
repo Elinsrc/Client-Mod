10f726170eaaf117cec829b10f17aef336546d3c
Elinsrc <99191833+Elinsrc@users.noreply.github.com>
2024-01-11 20:24:06 +0500

    Update MainActivity.java

--- android/app/src/main/java/su/xash/clientmod/MainActivity.java
+++ android/app/src/main/java/su/xash/clientmod/MainActivity.java
@@ -27,14 +27,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        try {
-            getPackageManager().getPackageInfo("su.xash.engine", 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=su.xash.engine")).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
-            finish();
-            return;
-        }
+        String pkg = "su.xash.engine.test";
 
+        try {
+            getPackageManager().getPackageInfo(pkg, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            try {
+                pkg = "su.xash.engine";
+                getPackageManager().getPackageInfo(pkg, 0);
+            } catch (PackageManager.NameNotFoundException ex) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=su.xash.engine")).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                finish();
+                return;
+            }
+        }
+        
         mActivityResultLauncher.launch(new Intent().setComponent(new ComponentName("su.xash.engine", "su.xash.engine.XashActivity"))
 //                don't set yet because it breaks getCallingPackage
 //                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
