f89e0a68c0c31e2b0b4487d1232edceac40a6721
Elinsrc <99191833+Elinsrc@users.noreply.github.com>
2025-10-19 10:38:05 +0500

    client:imgui_backend: use GL_CLAMP instead of GL_CLAMP_TO_EDGE

--- cl_dll/ui/IMGUI/imgui_backend.cpp
+++ cl_dll/ui/IMGUI/imgui_backend.cpp
@@ -229,8 +229,8 @@ void ImGui_ImplOpenGL2_UpdateTexture(ImTextureData* tex)
         glBindTexture(GL_TEXTURE_2D, gl_texture_id);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
         glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
         glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex->Width, tex->Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
